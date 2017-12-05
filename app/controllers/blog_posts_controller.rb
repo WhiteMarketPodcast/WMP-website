07abbc5b93b6edca70b2bff43ad8ac60b4f320c8
Ciaran <ciaran.edwards@live.com>
2017-12-05 00:36:57 +0000

    Check for localized blog posts first

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,17 @@
 class BlogPostsController < ApplicationController
 
   def index
-    @posts = BlogPost.paginate(page: params[:page], per_page: 10).order("id DESC")
+    posts = LocalizedBlogPost.where(locale: locale)
+    posts = BlogPost if posts.empty?
+
+    @posts = posts.paginate(page: params[:page], per_page: 10).order("id DESC")
   end
 
   def show
-    @post = BlogPost.find(params[:id])
+    @post = LocalizedBlogPost.where(
+              locale: locale,
+              blog_post_id: params[:id]
+            ).first
+    @post ||= BlogPost.find(params[:id])
   end
 end
