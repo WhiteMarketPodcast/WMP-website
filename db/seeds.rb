e0a42da7d79f1293befde4a13a80b491d11e8295
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-23 10:22:01 +0000

    Updated the seeds for blog posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-BlogPost.create(
+LocalizedBlogPost.create(
+  locale: :en,
+  blog_post: BlogPost.create,
   title: "Lorem ipsum dolor sit amet, consectetur adipisicing",
   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 )
 
-BlogPost.create(
+LocalizedBlogPost.create(
+  locale: :en,
+  blog_post: BlogPost.create,
   title: "Ut enim ad minim veniam, quis nostrud exercitation ullamco",
   content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 )
 
-BlogPost.create(
+LocalizedBlogPost.create(
+  locale: :en,
+  blog_post: BlogPost.create,
   title: "Duis aute irure dolor in reprehenderit in voluptate velit",
   content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 )
